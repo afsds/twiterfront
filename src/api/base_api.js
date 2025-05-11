bb2ce07b6fe5991f9053e969bab650419784f848
afsds <142832572+afsds@users.noreply.github.com>
2025-05-11 19:01:32 -0300

    Update base_api.js

--- src/api/base_api.js
+++ src/api/base_api.js
@@ -1,1 +1,9 @@
-export const API_BASE_URL = 'https://analice.pythonanywhere.com/';
+const express = require('express');
+const app = express();
+
+app.get('/api/mensagem', (req, res) => {
+    res.json({ mensagem: 'Olá, esta é sua API!' });
+});
+
+app.listen(3000, () => console.log('API rodando na porta 3000'));
+
